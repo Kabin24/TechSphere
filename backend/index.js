8e125fbba1d78a1cc7b8eaa3e9bfb0bdf31797eb
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2025-07-27 12:38:52 +0545

    url change

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://techsphere-f5b7.onrender.com',
     credentials: true
 }
 
